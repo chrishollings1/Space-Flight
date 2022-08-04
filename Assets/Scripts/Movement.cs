bde821173385976c225608f23ce6a4baec6e8531
DESKTOP-VDVB68J\User <chrisjhollings1@outlook.com>
2022-08-04 16:16:05 +0100

    up thrust

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+     void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up);
+        }
+    }
+
+     void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Presed A - Rotate Left ");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Presed D - Rotate Right ");
+        }
+    }
+}
